90fc00b6ff7736641013afaf1d6571c311eb2b76
arianne05 <quimpoarianne5@gmail.com>
2023-12-06 17:26:09 +0800

    Added new route,model and controller for senior citizen

---
+++ app/Http/Controllers/SeniorCitizenController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeniorCitizenController extends Controller
+{
+    public function index(){
+        return "Hello Senior Citizens";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeniorCitizenController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// CONTROLLER ROUTE FOR SENIOR CITIZEN
+Route::controller(SeniorCitizenController::class)->group(function(){
+    Route::get('/senior','index');
 });
