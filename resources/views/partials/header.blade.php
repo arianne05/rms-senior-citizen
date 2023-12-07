7daeea7d56937a306c4df0525da837dd08df575e
arianne05 <quimpoarianne5@gmail.com>
2023-12-07 18:42:05 +0800

    Added sweetalert

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Link--}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @vite('resources/css/app.css')
 
     <title>{{$title !="" ? $title : 'Student System'}}</title>
