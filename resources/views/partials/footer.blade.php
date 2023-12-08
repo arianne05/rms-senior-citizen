6e7ff9127a1d1871852cd921a056864163e242d9
arianne05 <quimpoarianne5@gmail.com>
2023-12-08 17:09:15 +0800

    Added data table script and jquery

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,31 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+
+{{-- DATATABLE --}}
+<script>
+    let table = new DataTable('#myTable');
+</script>
+  
+
+  {{-- ALERT SWAL --}}
+  <script>
+      const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+  Toast.fire({
+    icon: "success",
+    title: "{{$title}} {{$name}}"
+  });
+  
+  </script>
+
 </body>
 </html>
