2791c6535b0d4a592bd0cdee725f52213560bf56
arianne05 <quimpoarianne5@gmail.com>
2023-12-08 18:18:26 +0800

    Working layout and query display in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,50 @@
 @include('partials.header')
 
-
 <section class="flex">
-    @include('partials.sidebar')
+  {{-- Sidebar --}}
+  @include('partials.sidebar')
     
-    <div class="flex flex-col w-screen">
-      @include('partials.navbar')
-      <div class=""></div>
-      <h1>HI {{$alert}}</h1>
+  <div class="flex flex-col w-screen">
+    {{-- Navbar --}}
+    @include('partials.navbar')
+    
+    <h1 class="ml-7 font-semibold text-lg">Latest Added</h1>
+
+    {{-- Main Section --}}
+    <div class="container rounded-lg border-solid border-2 border-gray-100 max-w-5xl w-full h-auto m-7 p-8">
+
+      <table id="dashboardTbl" class="display">
+          <thead>
+              <tr>
+                  <th>Last Name</th>
+                  <th>First Name</th>
+                  <th>Middle Name</th>
+                  <th>Sex</th>
+                  <th>Birthdate</th>
+                  <th>Action</th>
+              </tr>
+          </thead>
+          <tbody>
+              @foreach ($seniors as $senior)  
+                <tr>
+                    <td>{{$senior->lastname}}</td>
+                    <td>{{$senior->firstname}}</td>
+                    <td>{{$senior->middlename}}</td>
+                    <td>{{$senior->sex}}</td>
+                    <td>{{$senior->birthdate}}</td>
+                    <td><button>Edit</button> <button>Delete</button></td>
+                </tr>
+              @endforeach
+          </tbody>
+      </table>
+
     </div>
-   
+
+  </div>
 </section>
 
-
-
-
-
-
-<script>
+  {{-- ALERT SWAL --}}
+  <script>
     const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
@@ -35,6 +62,5 @@ Toast.fire({
 });
 
 </script>
-
 
 @include('partials.footer')
