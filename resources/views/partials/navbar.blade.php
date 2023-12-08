8472df8a03e7afe5199a3ce7505e141844285cfa
arianne05 <quimpoarianne5@gmail.com>
2023-12-08 18:20:46 +0800

    Refine add btn and search

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,23 @@
 <nav class="flex justify-between p-5 align-center">
-    <h1 class="flex justify-start align-center py-2 px-4"><b>Dashboard</b></h1>
+    <h1 class="text-xl font-bold flex justify-start align-center py-2 px-4">Dashboard</h1>
 
     <div class="flex justify-end w-full space-x-4">
-        <input class="border-2 border-slate-100 placeholder-slate-400 rounded-lg w-1/5 py-2 px-4" placeholder="Search anything here ..." type="text">
+        
+        <label class="relative block">
+            
+            <span class="sr-only">Search</span>
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <span class="material-symbols-outlined text-slate-400">search</span>
+            </span>
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+        </label>
+        
         <ul class="flex space-x-4">
-            <li><button class="bg-sky-500 hover:bg-sky-700 rounded-lg py-2 px-4">Add New</button></li>
-            <li class="bg-green-100 rounded-full hover:bg-green-300 hover:text-white py-2 px-4">Me</li>
+            <li>
+                <button class="flex align-center font-medium text-slate-100 bg-sky-500 hover:bg-sky-700 rounded-lg py-2 px-8 mr-5">
+                    <span class="material-symbols-outlined">add</span>Add New
+                </button>
+            </li>
         </ul>
     </div>
     
