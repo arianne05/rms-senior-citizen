57689da3971a67ca0c21b82045b0e585370b9484
arianne05 <quimpoarianne5@gmail.com>
2023-12-10 15:59:07 +0800

    Added component for message pop up

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,20 @@
+@if(session()->has('message'))
+{{-- ALERT SWAL --}}
+<script>
+    const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+    }
+    });
+    Toast.fire({
+    icon: "success",
+    title: "{{session('message')}}"
+    });
+</script>
+@endif
