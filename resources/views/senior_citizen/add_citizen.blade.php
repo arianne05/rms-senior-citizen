85354fe855b6b249385f0ec0f80a0ccea2b942c5
arianne05 <quimpoarianne5@gmail.com>
2023-12-10 14:12:40 +0800

    Updated form with correspond name on db

--- resources/views/senior_citizen/add_citizen.blade.php
+++ resources/views/senior_citizen/add_citizen.blade.php
@@ -22,63 +22,80 @@
                     <div class="flex gap-3">
                          {{-- Lastname --}}
                          <div class="flex flex-col w-full">
-                            <label for="firstname" class="mb-2 mt-2 text-sm font-regular text-gray-500">Lastname <span class="font-bold text-red-600">*</span></label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <label for="lastname" class="mb-2 mt-2 text-sm font-regular text-gray-500">Lastname <span class="font-bold text-red-600">*</span></label>
+                            <input type="text" name="lastname" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                         {{-- Firstname --}}
                         <div class="flex flex-col w-full">
                             <label for="firstname" class="mb-2 mt-2 text-sm font-regular text-gray-500">Firstname <span class="font-bold text-red-600">*</span></label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="text" name="firstname" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                         {{-- Middlename --}}
                         <div class="flex flex-col w-full">
-                            <label for="firstname" class="mb-2 mt-2 text-sm font-regular text-gray-500">Middlename</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <label for="middlename" class="mb-2 mt-2 text-sm font-regular text-gray-500">Middlename</label>
+                            <input type="text" name="middlename" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
                     </div>
 
                     <div class="flex gap-3">
                         {{-- Suffix --}}
                         <div class="flex flex-col w-full">
-                            <label for="birthdate" class="mb-2 mt-2 text-sm font-regular text-gray-500">Suffix</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <label for="suffix" class="mb-2 mt-2 text-sm font-regular text-gray-500">Suffix</label>
+                            <select name="suffix" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                                <option value="" disabled selected>Select Suffix</option>
+                                <option value="Jr.">Jr.</option>
+                                <option value="Sr.">Sr.</option>
+                                <option value="I">I</option>
+                                <option value="II">II</option>
+                                <option value="III">III</option>
+                                <option value="IV">IV</option>
+                                <option value="IV">M.D</option>
+                            </select>
                         </div>
 
-                       {{-- Suffix --}}
+                       {{-- Civil Status --}}
                         <div class="flex flex-col w-full">
-                            <label for="birthdate" class="mb-2 mt-2 text-sm font-regular text-gray-500">Civil Status</label>
-                            <select class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <label for="civil_status" class="mb-2 mt-2 text-sm font-regular text-gray-500">Civil Status</label>
+                            <select name="civil_status" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                                 <option value="" disabled selected>Select Status</option>
                                 <option value="Single">Single</option>
                                 <option value="Married">Married</option>
+                                <option value="Divorced">Divorced</option>
+                                <option value="Widowed">Widowed</option>
                             </select>
                         </div>
                     </div>
 
                     {{-- Birthplace --}}
                     <div class="flex flex-col w-full">
-                        <label for="firstname" class="mb-2 mt-2 text-sm font-regular text-gray-500">Birthplace</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <label for="birthplace" class="mb-2 mt-2 text-sm font-regular text-gray-500">Birthplace</label>
+                        <input type="text" name="birthplace" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    </div>
+
+                    {{-- Contact --}}
+                    <div class="flex flex-col w-full">
+                        <label for="contact" class="mb-2 mt-2 text-sm font-regular text-gray-500">Contact</label>
+                        <input type="number" name="contact" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
 
                     <div class="flex gap-3">
                         {{-- Bdate --}}
                         <div class="flex flex-col w-full">
                             <label for="birthdate" class="mb-2 mt-2 text-sm font-regular text-gray-500">Birthdate</label>
-                            <input type="date" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="date" name="birthdate" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                         {{-- Religion --}}
                         <div class="flex flex-col w-full">
                             <label for="religion" class="mb-2 mt-2 text-sm font-regular text-gray-500">Religion</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="text" name="religion" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                         {{-- Sex --}}
                         <div class="flex flex-col align-center w-full">
-                            <label for="birthdate" class="mb-2 mt-2 text-sm font-regular text-gray-500">Sex</label>
+                            <label for="sex" class="mb-2 mt-2 text-sm font-regular text-gray-500">Sex</label>
                             <div class="flex items-center space-x-4 py-2">
                                 <input type="radio" id="male" name="sex" value="Male" class="text-sky-500 focus:ring-sky-500">
                                 <label for="male">Male</label>
@@ -95,21 +112,21 @@
 
                     {{-- Unit/House Number --}}
                     <div class="flex flex-col w-full">
-                        <label for="religion" class="mb-2 mt-2 text-sm font-regular text-gray-500">Unit/House Number</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <label for="house_number" class="mb-2 mt-2 text-sm font-regular text-gray-500">Unit/House Number</label>
+                        <input type="text" name="house_number" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
 
                     <div class="flex gap-3">
                         {{-- Barangay --}}
                         <div class="flex flex-col w-full">
                             <label for="barangay" class="mb-2 mt-2 text-sm font-regular text-gray-500">Barangay</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="text" name="barangay" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                          {{-- Municipality --}}
                          <div class="flex flex-col w-full">
                             <label for="municipality" class="mb-2 mt-2 text-sm font-regular text-gray-500">Municipality</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="text" name="municipality" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
                     </div>
 
@@ -117,13 +134,13 @@
                         {{-- Province --}}
                         <div class="flex flex-col w-full">
                             <label for="province" class="mb-2 mt-2 text-sm font-regular text-gray-500">Province</label>
-                            <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="text" name="province" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
 
                          {{-- Zip Code --}}
                          <div class="flex flex-col w-full">
                             <label for="zipcode" class="mb-2 mt-2 text-sm font-regular text-gray-500">Zip Code</label>
-                            <input type="number" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            <input type="number" name="zipcode" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                         </div>
                     </div>
 
@@ -135,26 +152,44 @@
                     {{-- GSIS --}}
                     <div class="flex flex-col w-full">
                         <label for="gsis" class="mb-2 mt-2 text-sm font-regular text-gray-500">GSIS</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <input type="text" name="gsis" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
 
                     {{-- Philhealth --}}
                     <div class="flex flex-col w-full">
                         <label for="philhealth" class="mb-2 mt-2 text-sm font-regular text-gray-500">Philhealth</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <input type="text" name="philhealth" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
 
                     {{-- Tin --}}
                     <div class="flex flex-col w-full">
                         <label for="tin" class="mb-2 mt-2 text-sm font-regular text-gray-500">Tin</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <input type="text" name="tin" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
 
                     {{-- SSS --}}
                     <div class="flex flex-col w-full">
                         <label for="sss" class="mb-2 mt-2 text-sm font-regular text-gray-500">SSS</label>
-                        <input type="text" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                        <input type="text" name="sss" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
                     </div>
+
+                    <br>
+                    <h1 class="font-semibold text-l mb-2" id="section3">Beneficiaries Detail</h1>
+                    <hr class="mb-3">
+
+                    {{-- Beneficiary --}}
+                    <div class="flex flex-col w-full">
+                        <label for="beneficiary" class="mb-2 mt-2 text-sm font-regular text-gray-500">Beneficiaries</label>
+                        <input type="text" name="beneficiary" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    </div>
+
+                    {{-- Contact Beneficiary --}}
+                    <div class="flex flex-col w-full">
+                        <label for="contact_beneficiary" class="mb-2 mt-2 text-sm font-regular text-gray-500">Beneficiaries Contact</label>
+                        <input type="number" name="contact_beneficiary" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    </div>
+
+
                 </div>
             </div>
             
@@ -178,26 +213,29 @@
                     </form>
                     <br>
 
-                   
                     {{-- Status --}}
-                    <label for="status" class="ml-2 mb-2 mt-2 text-sm font-semibold">Membership Status</label>
-                    <div class="flex flex-col">
-                        <div class="flex ml-2">
-                            <input type="checkbox" id="checkbox1" class="text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                            <label for="checkbox1" class="ml-2">Person w/ Disability (PWD)</label>
-                        </div>
-                        <div class="flex ml-2">
-                            <input type="checkbox" id="checkbox2" class="text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                            <label for="checkbox2" class="ml-2">Pension</label>
-                        </div>
-                        <div class="flex ml-2">
-                            <input type="checkbox" id="checkbox2" class="text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                            <label for="checkbox2" class="ml-2">Non-Pension</label>
+                    <div class="flex flex-col align-center ml-2 w-full">
+                        <label for="sex" class="mb-2 mt-2 text-sm font-regular text-gray-500">Membership Status</label>
+                        <div class="flex flex-col">
+                            <div class="flex items-center space-x-2">
+                                <input type="radio" name="status_membership" value="Person w/ Disability" class="text-sky-500 focus:ring-sky-500">
+                                <label for="status_membership">Person w/ Disability (PWD)</label>
+                            </div>
+                            
+                            <div class="flex items-center space-x-2">
+                                <input type="radio" name="status_membership" value="Pension" class="text-pink-500 focus:ring-pink-500">
+                                <label for="status_membership">Pension</label>
+                            </div>
+
+                            <div class="flex items-center space-x-2">
+                                <input type="radio" name="status_membership" value="Non-Pension" class="text-pink-500 focus:ring-pink-500">
+                                <label for="status_membership">Non-Pension</label>
+                            </div>
                         </div>
                     </div>
                       
                     <br>
-                    <label for="status" class="ml-2 mb-2 text-sm font-semibold">Navigate</label>
+                    <label for="sex" class="ml-2 mb-2 mt-2 text-sm font-regular text-gray-500">Navigation Pane</label>
                     <ul class="ml-2 mt-2">
                         <a href="#section1"><li class="hover:text-green-700">Personal Information</li></a>
                         <a href="#section2"><li class="hover:text-green-700">Complete Address</li></a>
