9347c03368f026d83437bc7a6a678c26136f7de7
arianne05 <quimpoarianne5@gmail.com>
2023-12-14 17:17:42 +0800

    Added error  handling in template

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -11,22 +11,43 @@
         {{-- Main Section --}}
         <div class="container flex flex-col w-full h-auto p-10">
             <div class="flex gap-x-4">
-                <div class="flex flex-col w-1/2 p-5">
+                <form action="/process_user_update/{{$userdetail->id}}}" method="POST" class="flex flex-col w-1/2 p-5">
+                    @csrf
                     <h1 class="font-bold text-xl">Personal Detail</h1>
                     <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Name</label>
                     <input type="text" name="name" value="{{$userdetail->name}}" class="w-4/2 border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    @error('name')
+                            <p class="text-red-500 text-xs p-2">
+                                {{$message}}
+                            </p>
+                    @enderror
 
                     <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Email</label>
                     <input type="text" name="email" value="{{$userdetail->email}}" class="w-4/2 border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    @error('email')
+                            <p class="text-red-500 text-xs p-2">
+                                {{$message}}
+                            </p>
+                    @enderror
 
-                    <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Password</label>
+                    <label class="mb-2 mt-2 text-sm font-regular text-gray-500">New Password</label>
                     <input type="password" name="password" value="" class="w-4/2 border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    @error('password')
+                            <p class="text-red-500 text-xs p-2">
+                                {{$message}}
+                            </p>
+                    @enderror
 
                     <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Confirm Password</label>
-                    <input type="password" name="confirmation_password" value="" class="w-4/2 border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    <input type="password" name="password_confirmation" value="" class="w-4/2 border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                    @error('password_confirmation')
+                            <p class="text-red-500 text-xs p-2">
+                                {{$message}}
+                            </p>
+                    @enderror
 
-                    <a href="#" class="w-4/2 font-medium text-slate-100 bg-green-700 hover:bg-green-500 rounded-xl p-3 px-12 mt-8 text-center">Save Changes</a>
-                </div>
+                    <button type="submit" class="w-4/2 font-medium text-slate-100 bg-green-700 hover:bg-green-500 rounded-xl p-3 px-12 mt-8 text-center">Save Changes</button>
+                </form>
                 <div class="w-1/2 h-auto border-2 border-solid border-gray-200 p-5 rounded-xl">
                     <h1 class="font-bold text-xl">User Overview</h1>
                     <div class="flex flex-col items-center justify-center">
