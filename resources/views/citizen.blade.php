26567799cd5bb9299230b15436c14b58014f8d3f
arianne05 <quimpoarianne5@gmail.com>
2023-12-15 14:36:55 +0800

    Added message component after delete successfully

--- resources/views/citizen.blade.php
+++ resources/views/citizen.blade.php
@@ -164,6 +164,8 @@
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, config);
 </script>
-  
+
+{{-- Component --}}
+<x-message />
 
 @include('partials.footer')
