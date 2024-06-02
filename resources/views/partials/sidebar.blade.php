410231e2d13c1b36cdae145fd8d601c4cd76cacd
arianne05 <quimpoarianne5@gmail.com>
2024-06-02 22:19:23 +0800

    Logout is now beside the contents of sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -46,11 +46,7 @@
                                 <span class="material-symbols-outlined pr-2">person</span>Manage Account
                             </li>
                         </a>
-                    </div>
 
-                    <br><br><br><br><br><br><br><br><br><br><br> <!--Temporary-->
-                    <div class="p-4">
-                        <img src="{{asset('img/bg2.jpg')}}" class="w-full h-30 rounded" alt="">
                         <li class="flex align-center mb-2 mt-2 rounded-lg py-2.5 px-7 bg-[#f3b0c6] group hover:bg-[#82083C] hover:text-white">
                             <form action="/logout" id="logoutform" method="POST">
                                 @csrf
@@ -58,6 +54,16 @@
                             </form>
                         </li>
                     </div>
+
+                    {{-- <div class="p-4">
+                        <img src="{{asset('img/bg2.jpg')}}" class="w-full h-30 rounded" alt="">
+                        <li class="flex align-center mb-2 mt-2 rounded-lg py-2.5 px-7 bg-[#f3b0c6] group hover:bg-[#82083C] hover:text-white">
+                            <form action="/logout" id="logoutform" method="POST">
+                                @csrf
+                                <button type="submit" id="logoutbtn" class="flex align-center"><span class="material-symbols-outlined pr-2">logout</span>Logout</button>
+                            </form>
+                        </li>
+                    </div> --}}
                 </div>
             </div>
         </ul>
