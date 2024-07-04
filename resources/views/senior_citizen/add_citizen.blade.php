47713c3a18bff7253663c5a4b9cb26f5f7643f6d
arianne05 <quimpoarianne5@gmail.com>
2024-07-04 21:27:38 +0800

    add dropdown for religion

--- resources/views/senior_citizen/add_citizen.blade.php
+++ resources/views/senior_citizen/add_citizen.blade.php
@@ -133,12 +133,32 @@
                         {{-- Religion --}}
                         <div class="flex flex-col w-full">
                             <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Religion <span class="font-bold text-red-600">*</span></label>
-                            <input type="text" name="religion" value="{{old('religion')}}" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            {{-- <input type="text" name="religion" value="{{old('religion')}}" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"> --}}
+                            <select name="religion" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                                <option value="" disabled {{ old('religion') == '' ? 'selected' : '' }}>Select Religion</option>
+                                <option value="Christian" {{ old('religion') == 'Single' ? 'selected' : '' }}>Christian</option>
+                                <option value="INC" {{ old('religion') == 'Single' ? 'selected' : '' }}>INC</option>
+                                <option value="Muslim" {{ old('religion') == 'Single' ? 'selected' : '' }}>Muslim</option>
+                                <option value="Roman Catholic" {{ old('religion') == 'Single' ? 'selected' : '' }}>Roman Catholic</option>
+                                <option value="Other" {{ old('religion') == 'Single' ? 'selected' : '' }}>Other</option>
+                            </select>
                             @error('religion')
                                 <p class="text-red-500 text-xs p-2">
                                     {{$message}}
                                 </p>
                             @enderror    
+
+                        </div>
+
+                        <div class="flex flex-col w-full">
+                            <label class="mb-2 mt-2 text-sm font-regular text-gray-500">Other Religion</label>
+                            <input type="text" name="other_religion" value="{{old('religion')}}" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                            @error('other_religion')
+                                <p class="text-red-500 text-xs p-2">
+                                    {{$message}}
+                                </p>
+                            @enderror    
+
                         </div>
 
                         {{-- Sex --}}
