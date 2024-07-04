c521824baa0a4fa4bfd40e62cb6b9d2246d82a08
arianne05 <quimpoarianne5@gmail.com>
2024-07-04 22:16:40 +0800

    Changed save label to view in button

--- resources/views/barangay.blade.php
+++ resources/views/barangay.blade.php
@@ -23,7 +23,7 @@
                             <input type="date" value="{{ $dateto ?? '' }}" name="dateto" class="w-auto border border-slate-300 rounded-xl py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mb-2">
                         </div>
                         <div class="">
-                            <button type="submit" class="font-medium text-slate-100 bg-gray-400 hover:bg-blue-500 rounded-xl p-3 px-12">Save</button>
+                            <button type="submit" class="font-medium text-slate-100 bg-gray-400 hover:bg-blue-500 rounded-xl p-3 px-12">View</button>
                         </div>
                     </div>
                 </form>
